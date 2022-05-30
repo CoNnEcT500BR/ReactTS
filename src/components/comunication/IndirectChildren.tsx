63738e888ba224cf8e8d85cfd2e2f1f58fd2b9ca
connect500br <diegocaldi@outlook.com>
2022-05-30 16:44:45 -0300

    useState Communication Indirect

--- src/components/comunication/IndirectChildren.tsx
+++ src/components/comunication/IndirectChildren.tsx
@@ -7,10 +7,13 @@ type TIndirectChildrenProps = {
 export default function IndirectChildren({
   whenClick,
 }: TIndirectChildrenProps) {
+  const genAge = () => Math.floor(Math.random() * 20) + 50;
+  const genNerd = () => Math.random() > 0.5;
+
   return (
     <div>
       <div>Filho</div>
-      <button onClick={(_) => whenClick("João", 53, false)}>
+      <button onClick={(_) => whenClick("João", genAge(), genNerd())}>
         Fornecer Informações
       </button>
     </div>
--- src/components/comunication/IndirectFather.tsx
+++ src/components/comunication/IndirectFather.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import IndirectChildren from "./IndirectChildren";
 
 export default function IndirectFather() {
+  let [name, setName] = useState("?");
+  let [age, setAge] = useState(0);
+  let [nerd, setNerd] = useState(false);
+
   function Info(name: string, age: number, nerd: boolean) {
-    console.log(name, age, nerd);
+    setName(name);
+    setAge(age);
+    setNerd(nerd);
   }
 
   return (
     <div>
-      <div>Pai</div>
+      <div>
+        <span>{name} </span>
+        <span>
+          <strong>{age}</strong>
+        </span>
+        <span> {nerd ? "Verdadeiro" : "Falso"}</span>
+      </div>
       <IndirectChildren whenClick={Info}></IndirectChildren>
     </div>
   );
